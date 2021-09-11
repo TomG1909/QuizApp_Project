5eb9960806fe2b9ae3c065bcdc5c740bbcf32e27
Toma Gela <toma.gela@gmail.com>
2021-09-11 14:10:14 +0200

    Final version

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ let questions = [{
     }
 
 ]
+
 let currentQuestion = 0;
 let rightQuestions = 0;
 let audio_success = new Audio('Audio/success.mp3');
@@ -43,11 +44,11 @@ let audio_fail = new Audio('Audio/fail.mp3');
 function init() {
 
     showStartScreen();
+    resetProgressbarAndLinks()
 }
 
 
 function hideStartScreen() {
-
     document.getElementById('start').classList.add('d-none');
     document.getElementById('quizBody').style = '';
     document.getElementById('startlink').classList.remove('active-link');
@@ -58,33 +59,26 @@ function hideStartScreen() {
 }
 
 function showStartScreen() {
-
     document.getElementById('start').classList.remove('d-none');
     document.getElementById('quizBody').style = 'display: none';
     document.getElementById('endscreen').style = 'display: none';
     document.getElementById('startlink').classList.add('active-link');
     document.getElementById('scorelink').classList.add('disabled-link');
-
-
-
-
 }
+
 
 function showQuestion() {
 
     if (gameIsOver()) {
         showEndScreen();
-
     } else {
         updateProgressBar();
         updateToNextQuestion();
-
     }
 }
 
 
 function showEndScreen() {
-
     document.getElementById('endscreen').style = '';
     document.getElementById('quizBody').style = 'display: none';
     document.getElementById('amountOfQuestions').innerHTML = questions.length;
@@ -121,45 +115,56 @@ function gameIsOver() {
     return currentQuestion >= questions.length;
 }
 
+
 function answer(selection) {
-    let question = questions[currentQuestion]; //Entsprechende zugehörige Frage an der Stelle 0 gewählt
-    let selectedQuestionNumber = selection.slice(-1); //Es wird auf die letzte Stelle des Strings zugegriffen, hier: answer_X
-    let idOfRightAnswer = `answer_${question['right_answer']}`;
-
-
-    if (selectedQuestionNumber == question['right_answer']) {
-
-        document.getElementById(selection).parentNode.parentNode.classList.add('bg-green');
-        document.getElementById(selection).parentNode.classList.remove('text-secondary');
-        audio_success.play();
-        rightQuestions++;
-
+    if (rightAnswerSelected(selection)) {
+        rightAnswerLogic(selection);
     } else {
-
-        document.getElementById(selection).parentNode.parentNode.classList.add('bg-red');
-        document.getElementById(selection).parentNode.classList.remove('text-secondary');
-        document.getElementById(idOfRightAnswer).parentNode.parentNode.classList.add('bg-green');
-        document.getElementById(idOfRightAnswer).parentNode.classList.remove('text-secondary');
-
-        audio_fail.play();
+        wrongAnswerLogic(selection);
     }
     document.getElementById('nextbutton').disabled = false;
-
 
 }
 
 
-/*function rightAnswerSelected(selectedQuestionNumber) {
+function rightAnswerSelected(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1); //Es wird auf die letzte Stelle des Strings zugegriffen, hier: answer_X
+
     return selectedQuestionNumber == question['right_answer'];
-}*/
+}
+
+
+function rightAnswerLogic(selection) {
+    document.getElementById(selection).parentNode.parentNode.classList.add('bg-green');
+    document.getElementById(selection).parentNode.classList.remove('text-secondary');
+    audio_success.currentTime = 0;
+    audio_success.play();
+    rightQuestions++;
+
+}
+
+
+function wrongAnswerLogic(selection) {
+    let question = questions[currentQuestion];
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    document.getElementById(selection).parentNode.parentNode.classList.add('bg-red');
+    document.getElementById(selection).parentNode.classList.remove('text-secondary');
+    document.getElementById(idOfRightAnswer).parentNode.parentNode.classList.add('bg-green');
+    document.getElementById(idOfRightAnswer).parentNode.classList.remove('text-secondary');
+    audio_fail.currentTime = 0;
+    audio_fail.play();
+}
+
 
 function nextQuestion() {
     currentQuestion++;
     document.getElementById('nextbutton').disabled = true;
     showQuestion();
     resetAnswers();
-
 }
+
 
 function resetAnswers() {
     document.getElementById('answer_1').parentNode.parentNode.classList.remove('bg-red');
@@ -179,11 +184,18 @@ function resetAnswers() {
     document.getElementById('answer_4').parentNode.classList.add('text-secondary');
 }
 
+function resetProgressbarAndLinks() {
+    document.getElementById('startlink').classList.add('active-link');
+    document.getElementById('scorelink').classList.add('disabled-link');
+    document.getElementById('scorelink').classList.remove('active-link');
+    document.getElementById('quizlink').classList.remove('active-link');
 
+    document.getElementById('progress-bar').innerHTML = '';
+    document.getElementById('progress-bar').style = '';
+}
 
 
 function restartGame() {
-
     document.getElementById('endscreen').style = 'display: none';
     document.getElementById('quizBody').style = '';
 
@@ -191,13 +203,5 @@ function restartGame() {
     rightQuestions = 0;
 
     init();
-    //Verhalten der Menu-links
-    document.getElementById('startlink').classList.add('active-link');
-    document.getElementById('scorelink').classList.add('disabled-link');
-    document.getElementById('scorelink').classList.remove('active-link');
-    document.getElementById('quizlink').classList.remove('active-link');
-    //Progressbar zurückgesetzt
-    document.getElementById('progress-bar').innerHTML = '';
-    document.getElementById('progress-bar').style = '';
 
 }
